15635146e2cbbb740f9de87e21d04325e16f66c2
Losvaize <Vartazian37@gmail.com>
2025-10-04 16:33:44 -0700

    Added Generators

---
+++ Generators.py
@@ -0,0 +1,33 @@
+class Generator():
+    weight: int
+    energy_output: int
+    maximum_charge: int
+    charge_redzone: int
+
+    def __init__(self, 
+                 _id: int, 
+                 name: str, 
+                 part_type:str, 
+                 price: int,
+                 weight: int, 
+                 energy_output: int, 
+                 maximum_charge: int,
+                 charge_redzone: int):
+        self.id = _id
+        self.name = name
+        self.part_type = part_type
+        self.price = price
+        self.weight = weight
+        self.energy_output = energy_output
+        self.maximum_charge = maximum_charge
+        self.charge_redzone = charge_redzone
+
+all_generators = (
+    Generator(0x3D, "GPS-VVA", "PULSE GENERATOR", 19500, 308, 4728, 28000, 7800), #Low in both power and capacity. Wide red zone.
+	Generator(0x3E, "GPS-V6", "PULSE GENERATOR", 32000, 363, 4728, 43000, 5000), #Load increased to nearly twice that of the GPS-VVA.
+	Generator(0x3F, "GRD-RX5", "PULSE GENERATOR", 23300, 225, 5300, 38000, 4000), #Balanced-performance generator.
+	Generator(0x40, "GRD-RX6", "PULSE GENERATOR", 27800, 286, 6000, 33000, 4000), #Performance not bad, but the equipment is so-so.
+	Generator(0x41, "GRD-RX7", "PULSE GENERATOR", 38700, 348, 6810, 31500, 5000), #Very good power but poor stamina.
+	Generator(0x42, "GBG-10000", "PULSE GENERATOR", 43500, 398, 9988, 34000, 2980), #High Power provides a wide selection of equipment.
+	Generator(0x43, "GBG-XR", "PULSE GENERATOR", 56000, 452, 8207, 48000, 3250), #Custom-made unit having both power and capacity.
+    )
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#Import all Parts from Databases
 import random
 import Legs
 import Cores
+import Generators
 
 def shuffle_random_leg ():
     random_legs = random.choice(Legs.all_legs)
@@ -23,20 +25,29 @@ def shuffle_random_core ():
     random_core = random.choice(Cores.all_cores)
     return(random_core)
 
+def shuffle_random_generator():
+    random_generator = random.choice(Generators.all_generators)
+    return(random_generator)
+
 def main():
     #Defining each maximum part value
     ac_remaining_weight = 0
     ac_core_weight = 0
-    #ac_total_energy = 0
+    ac_remaining_energy = 0
     
     ac_legs = shuffle_random_leg ()
     ac_core = shuffle_random_core ()
-
-    ac_remaining_weight = ac_legs.max_weight - ac_core.weight
+    ac_generator = shuffle_random_generator()
+    
+    
+    ac_remaining_weight = ac_legs.max_weight - ac_core.weight - ac_generator.weight
     ac_core_weight = ac_core.max_weight
+    ac_remaining_energy = ac_generator.energy_output - ac_core.energy_drain - ac_legs.energy_drain 
 
-    print(ac_legs.name, ac_legs.max_weight, ac_remaining_weight, ac_core.name, ac_core.weight)
-
+    print(ac_legs.name, f"Max Weight:", ac_legs.max_weight)
+    print(ac_core.name, f"Arm Weight Remaining:", ac_core_weight ) 
+    print(ac_generator.name)
+    print(f"Remaining Weight:", ac_remaining_weight, f"Remaining Arm Weight:", ac_core_weight, f"Remaining Energy:", ac_remaining_energy )
 
 if __name__ == "__main__":
     main()
