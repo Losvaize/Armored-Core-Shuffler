63b0685f8c8163cbd8cb4644e947067901f162e4
Losvaize <Vartazian37@gmail.com>
2025-10-14 20:40:23 -0700

    Added Back Weapon Logic.

--- Parts.py
+++ Parts.py
@@ -497,8 +497,8 @@ all_back_weapons = (
     Back_Weapon(0x62, "WM-MVG802", "MISSILE", 44000, 718, 220, "NONE", "STANDARD", 1560, 32, "SOLID", 252, 10000, 2, 10, False, 2), #Fires up to 2 missiles at once.
     Back_Weapon(0x63, "WM-L201", "LARGE MISSILE", 46200, 835, 180, "NONE", "STANDARD", 4300, 12, "SOLID", 897, 12500, 1, 10, False, 3), #Powerful large missiles fired singly
     Back_Weapon(0x64, "WM-X201", "MULTI MISSILE", 62250, 720, 250, "NONE", "STANDARD", 980, 18, "SOLID", 1125, 12000, 1, 15, False, 3), #Multi-warhead missile that scatters warheads in flight.
-    Back_Weapon(0x65, "WM-X5-AA", "BOMB DISPENSER", 19300, 616, 85, "NONE", "NONE", 675, 10, "SOLID", 270, 0, 0, 50, True, 1), #Drops 8 ground-attack mines. For experts.
-    Back_Weapon(0x66, "WM-X10", "BOMB DISPENSER", 24800, 939, 105, "NONE", "NONE", 675, 10, "SOLID", 560, 0, 0, 50, True, 1), #Drops 16 powerful ground-attack mines.
+    Back_Weapon(0x65, "WM-X5-AA", "BOMB DISPENSER", 19300, 616, 85, "NONE", "NONE", 675, 10, "SOLID", 270, 0, 0, 50, True, 2), #Drops 8 ground-attack mines. For experts.
+    Back_Weapon(0x66, "WM-X10", "BOMB DISPENSER", 24800, 939, 105, "NONE", "NONE", 675, 10, "SOLID", 560, 0, 0, 50, True, 2), #Drops 16 powerful ground-attack mines.
     Back_Weapon(0x67, "WM-P40001", "DUAL MISSILE", 43800, 755, 320, "NONE", "STANDARD", 830, 60, "SOLID", 130, 9000, 1, 10, False, 2), #Fires 2 left or right curving indirect attack missiles.
     Back_Weapon(0x68, "WM-PS-2", "TRIPLE MISSILE", 66700, 1125, 360, "NONE", "STANDARD", 830, 90, "SOLID", 130, 9000, 1, 10, False, 2), #Fires 3 up-curving indirect attack missiles.
     Back_Weapon(0x69, "WR-S50", "SMALL ROCKET", 15900, 218, 8, "NONE", "NONE", 1310, 50, "SOLID", 110, 12500, 0, 8, True, 1), #Carries 50 small rockets.
@@ -513,13 +513,13 @@ all_back_weapons = (
     Back_Weapon(0x72, "WC-XP4000", "PULSE CANNON", 61000, 318, 364, "NONE", "NARROW & DEEP", 770, 100, "ENERGY", 0, 9000, 1, 5, True, 3), #Energy weapon. Reloading ion cannon.
     Back_Weapon(0x73, "WC-XC8000", "LASER CANNON", 78700, 1110, 455, "NONE", "NARROW & DEEP", 2065, 50, "ENERGY", 0, 8500, 1, 10, True, 4), #Energy weapon. Fires laser rounds.
     Back_Weapon(0x74, "WC-01QL", "PLASMA CANNON", 69500, 273, 618, "NONE", "NARROW & DEEP", 1531, 80, "ENERGY", 0, 12000, 1, 7, True, 4), #Energy weapon. Beam cuts down enemies.
-    Back_Weapon(0x75, "RXA-01WE", "RADAR", 12100, 210, 243, "PROVIDED", 8650, "STANDARD"), #Old-style antenna but still holds up well in use.
-    Back_Weapon(0x76, "RZ-A0", "RADAR", 17900, 480, 387, "PROVIDED", 11500, "CIRCLE"), #This radar uses 2 dishes for enhanced enemy-search capability.
-    Back_Weapon(0x77, "RXA-99", "RADAR", 14500, 160, 267, "PROVIDED", 8800, "STANDARD"), #New-type radar permits even wider area to be searched.
-    Back_Weapon(0x78, "RXA-77", "RADAR", 23000, 125, 274, "PROVIDED", 8700, "STANDARD"), #This radar can detect the approach of homing missiles.
-    Back_Weapon(0x79, "RZ-A1", "RADAR", 33000, 433, 403, "PROVIDED", 15700, "CIRCLE"),  #Expands the enemy-search range up to the current technological limit.
-    Back_Weapon(0x7A, "RZT-333", "RADAR", 27700, 343, 451, "PROVIDED", 11700, "OCTAGON"), #Combines both missile detection and wide-range search capability.
-    Back_Weapon(0x7B, "RZ-BBP", "RADAR", 40900, 454, 566, "PROVIDED", 16300, "CIRCLE"), #Highest-quality radar with highest-class performance.
+    Back_Weapon(0x75, "RXA-01WE", "RADAR", 12100, 210, 243, "PROVIDED", 8650, "STANDARD", fire_unlocked=False, tier = 1), #Old-style antenna but still holds up well in use.
+    Back_Weapon(0x76, "RZ-A0", "RADAR", 17900, 480, 387, "PROVIDED", 11500, "CIRCLE", fire_unlocked=False, tier = 1), #This radar uses 2 dishes for enhanced enemy-search capability.
+    Back_Weapon(0x77, "RXA-99", "RADAR", 14500, 160, 267, "PROVIDED", 8800, "STANDARD", fire_unlocked=False, tier = 1), #New-type radar permits even wider area to be searched.
+    Back_Weapon(0x78, "RXA-77", "RADAR", 23000, 125, 274, "PROVIDED", 8700, "STANDARD", fire_unlocked=False, tier = 1), #This radar can detect the approach of homing missiles.
+    Back_Weapon(0x79, "RZ-A1", "RADAR", 33000, 433, 403, "PROVIDED", 15700, "CIRCLE", fire_unlocked=False, tier = 1),  #Expands the enemy-search range up to the current technological limit.
+    Back_Weapon(0x7A, "RZT-333", "RADAR", 27700, 343, 451, "PROVIDED", 11700, "OCTAGON", fire_unlocked=False, tier = 2), #Combines both missile detection and wide-range search capability.
+    Back_Weapon(0x7B, "RZ-BBP", "RADAR", 40900, 454, 566, "PROVIDED", 16300, "CIRCLE", fire_unlocked=False, tier = 2), #Highest-quality radar with highest-class performance.
     Back_Weapon(0x7C, "WX-S800/2", "DUAL MISSILE", 69400, 1650, 415, "NONE", "STANDARD", 1120, 60, "SOLID", 515, 11000, 1, 12, False, 3), #Fires 2 missiles with 1 lock-on
     Back_Weapon(0x7D, "WX-S800-GF", "DUAL MISSILE", 90900, 1110, 656, "NONE", "STANDARD", 1120, 60, "SOLID", 515, 11000, 1, 10, False, 3), #Fires 6 missiles with 1 lock-on
     Back_Weapon(0x7E, "XCS-9900", "MULTI MISSILE", 94500, 1480, 310, "NONE", "STANDARD", 980, 20, "SOLID", 1125, 12000, 1, 15, False, 3) #Fires 2 multi-warhead missiles simultaneously.
@@ -635,6 +635,11 @@ all_arm_weapon_l = (
     Arm_Weapon_L(0x91, "LS-3303", "LASERBLADE", 37200, 224, 43, 2630, 1210, 2), #Enhanced blade weapon. Both power and energy consumption are higher.
     Arm_Weapon_L(0x92, "LS-99-MOONLIGHT", "LASERBLADE", 54000, 336, 93, 810, 2801, 3) #Blade weapon with more than twice the power of conventional blades.
 )
+Arm_Weapon_L_by_tier = {
+    1: [Arm_Weapon_L for Arm_Weapon_L in all_arm_weapon_l if Arm_Weapon_L.tier == 1],
+    2: [Arm_Weapon_L for Arm_Weapon_L in all_arm_weapon_l if Arm_Weapon_L.tier == 2],
+    3: [Arm_Weapon_L for Arm_Weapon_L in all_arm_weapon_l if Arm_Weapon_L.tier == 3],
+}
 
 
 #all_parts: typing.Tuple[Part, ...] = all_heads + all_cores + all_legs + all_arms + all_arm_weapon_r + all_back_weapons + all_arm_weapon_l + all_boosters + all_fcs + all_generators + all_options_parts
--- Tier Generation Tests.py
+++ Tier Generation Tests.py
@@ -127,6 +127,77 @@ def get_random_FCS_by_tier():
 
     return random.choice(eligible_FCS)
 
+def get_random_Arm_Weapon_R_by_tier():
+    tiers = [1, 2, 3, 4]
+    tier_weights = [50, 35, 15, 0]
+
+    selected_tier = random.choices(tiers, weights = tier_weights, k=1)[0]
+
+    if selected_tier == 1:
+        eligible_Arm_r = Parts.Arm_Weapon_R_by_tier[1]
+    elif selected_tier == 2:
+        eligible_Arm_r = Parts.Arm_Weapon_R_by_tier[2]
+    elif selected_tier == 3:
+        eligible_Arm_r = Parts.Arm_Weapon_R_by_tier[3]
+    elif selected_tier == 4:
+        eligible_Arm_r = Parts.all_arm_weapon_r
+    else:
+        return None
+
+    return random.choice(eligible_Arm_r)
+
+def get_random_Arm_Weapon_L_by_tier():
+    tiers = [1, 2, 3]
+    tier_weights = [50, 35, 15]
+
+    selected_tier = random.choices(tiers, weights = tier_weights, k=1)[0]
+
+    if selected_tier == 1:
+        eligible_Arm_l = Parts.Arm_Weapon_L_by_tier[1]
+    elif selected_tier == 2:
+        eligible_Arm_l = Parts.Arm_Weapon_L_by_tier[2]
+    elif selected_tier == 3:
+        eligible_Arm_l = Parts.Arm_Weapon_L_by_tier[3]
+    elif selected_tier == 4:
+        eligible_Arm_l = Parts.all_arm_weapon_l
+    else:
+        return None
+
+    return random.choice(eligible_Arm_l)
+
+def get_Back_Weapon_by_tier(guarantee_unlocked):
+    tiers = [1, 2, 3, 4]
+    tier_weights = [50, 25, 15, 10]
+      
+    selected_tier = random.choices(tiers, weights = tier_weights, k=1)[0]
+
+    if selected_tier == 1:
+        eligible_Back_Weapon = Parts.Back_Weapon_by_tier[1]
+    elif selected_tier == 2:
+        eligible_Back_Weapon = Parts.Back_Weapon_by_tier[2]
+    elif selected_tier == 3:
+        eligible_Back_Weapon = Parts.Back_Weapon_by_tier[3]
+    elif selected_tier == 4:
+        eligible_Back_Weapon = Parts.all_back_weapons
+    else:
+        return None
+
+    back_weapon = random.choice(eligible_Back_Weapon)
+
+    if guarantee_unlocked:
+        if back_weapon.fire_unlocked == False:
+            back_weapon_inelgible = True
+        else:
+            back_weapon_inelgible = False
+        while (back_weapon_inelgible):
+            back_weapon = random.choice(eligible_Back_Weapon)
+            if back_weapon.fire_unlocked == False:
+                back_weapon_inelgible = True
+            else:
+                back_weapon_inelgible = False
+    
+    return back_weapon
+
 
 def main():
     ac_head = get_random_head_by_tier()
@@ -136,6 +207,14 @@ def main():
     ac_booster = get_random_booster_by_tier()
     ac_generator = get_random_generator_by_tier()
     ac_FCS = get_random_FCS_by_tier()
+    ac_arm_weapon_r = get_random_Arm_Weapon_R_by_tier()
+    ac_arm_weapon_l = get_random_Arm_Weapon_L_by_tier()
+
+
+    percentage_chance_r = 50
+    percentage_chance_l = 50
+    percentage_chance_back_r = 50
+    percentage_chance_back_l = 50
 
     ac_remaining_weight = ac_legs.max_weight - ac_core.weight - ac_generator.weight - ac_arms.weight - ac_booster.weight - ac_head.weight - ac_FCS.weight
 
@@ -143,13 +222,70 @@ def main():
 
     ac_remaining_energy = ac_generator.energy_output - ac_core.energy_drain - ac_legs.energy_drain - ac_arms.energy_drain - ac_booster.energy_drain - ac_head.energy_drain - ac_FCS.energy_drain
     
+    if  ac_arms.humanoid_arm == True:
+        if  0 <= percentage_chance_r <= 100:
+            ac_arm_weapon_r = get_random_Arm_Weapon_R_by_tier()
+        if random.random() < percentage_chance_r / 100:
+                ac_remaining_weight -= ac_arm_weapon_r.weight
+        else:
+            ac_arm_weapon_r.name = "NO EQUIP"
+
+        if 0 <= percentage_chance_l <= 100:
+            if ac_arm_weapon_r.name == "NO EQUIP":
+                percentage_chance_l = 100
+            ac_arm_weapon_l = get_random_Arm_Weapon_L_by_tier()
+            if random.random() < percentage_chance_l / 100:
+                ac_remaining_weight -= ac_arm_weapon_l.weight
+            else:
+                ac_arm_weapon_l.name = "NO EQUIP"
+
+    if ac_arms.humanoid_arm == False: #Flag for Weapon names 
+        ac_arm_weapon_r.name = "EQUIPMENT IMPOSSIBLE"
+        ac_arm_weapon_l.name = "EQUIPMENT IMPOSSIBLE"
+
+
+# I 100% chance want a back weapon when I have less than 2 Arm weapons or I have Gun arms.
+# I want a 50% chance of a Back weapon when I have 2 Weapons.
+
+    if ac_arm_weapon_l.name or ac_arm_weapon_r.name == "NO EQUIP" or "EQUIPMENT IMPOSSIBLE":
+        percentage_chance_back_r = 100
+    ac_back_weapon_r = get_Back_Weapon_by_tier(guarantee_unlocked = percentage_chance_back_r == 100)
+    if random.random() < percentage_chance_back_r / 100:
+        ac_remaining_weight -= ac_back_weapon_r.weight
+    else : ac_back_weapon_r.name = "NO EQUIP"
+        
+# I want a 25% chance of a 2nd back weapon when I have any of the above (back weapon L)
+
+    if ac_arm_weapon_r == True:
+        ac_remaining_energy -= ac_arm_weapon_r.energy_drain
+
+    if ac_arm_weapon_l == True:
+        ac_remaining_energy -= ac_arm_weapon_l.energy_drain
+    if ac_back_weapon_r == True:
+        ac_remaining_energy -= ac_back_weapon_r.energy_drain
+    #if ac_back_weapon_l == True:
+    #    ac_remaining_energy -= ac_back_weapon_l.energy_drain
+
+    if ac_arms.humanoid_arm == False: #Flag for Weapon names 
+        ac_arm_weapon_r.name = "EQUIPMENT IMPOSSIBLE"
+        ac_arm_weapon_l.name = "EQUIPMENT IMPOSSIBLE"
+
+    if (ac_remaining_weight < 0 or ac_core_weight < 0 or ac_remaining_energy < 0):
+        invalid_AC = True
+
     print (ac_head.name)
     print (ac_core.name)
     print (ac_arms.name)
     print (ac_legs.name)
     print (ac_booster.name)
     print (ac_generator.name)
-    print (ac_FCS.name) 
+    print (ac_FCS.name)
+    print (ac_arm_weapon_r.name)
+    print (ac_arm_weapon_l.name)
+    print (ac_back_weapon_r.name)
+
+#Todd wanted me to Loop this over so that all the parameters are met for a valid AC project for later.
+
     print(f"Remaining Weight:", ac_remaining_weight, f"Remaining Arm Weight:", ac_core_weight, f"Remaining Energy:", ac_remaining_energy)
 
 if __name__ == "__main__":
